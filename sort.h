53b1d0617effc91e007da708cb5d02a7d2ea4905
zenny <136650902+skilogistics@users.noreply.github.com>
2023-11-18 21:59:36 -0800

    Updating sort.h

--- sort.h
+++ sort.h
@@ -29,4 +29,11 @@ void swap(int *a, int *b);
 void recur(int *array, int low, int high);
 int partition(int *array, int low, int high);
 void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
