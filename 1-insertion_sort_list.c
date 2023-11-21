e48abfbfdca00e1160145040e6d84eb1df208dd7
zenny <136650902+skilogistics@users.noreply.github.com>
2023-11-21 08:41:13 -0800

    modifying insertion_sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,34 +1,48 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sort and prnts double linked list
- * using the Insertion sort algorithm.
- * @list: Pointer to the head of the list.
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list;
-	listint_t *next_node;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	while (current != NULL)
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		next_node = current->next;
-		while (current->prev != NULL && current->n < current->prev->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			current->prev->next = current->next;
-			current->next = current->prev;
-			current->prev = current->prev->prev;
-			current->next->prev = current;
-			if (current->prev != NULL)
-				current->prev->next = current;
-			else
-				*list = current;
-			print_list(*list);
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		current = next_node;
 	}
 }
